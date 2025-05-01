c7ff7dc6e66679ed79c0d30c7c4d90eab7147558
zomtec2311 <wtoedt@gmail.com>
2025-05-01 20:18:23 +0200

    Add files via upload

--- l10n/de.js
+++ l10n/de.js
@@ -4,6 +4,7 @@ OC.L10N.register(
     "How many lines to display" : "Wieviele Zeilen sollen angezeigt werden",
     "How many characters should the error reason display" : "Wieviele Zeichen soll der Fehlergrund anzeigen",
     "Delete dublicates" : "Dubletten löschen",
+    "_Delete %n dublicate_::_Delete %n dublicates_" : ["%n Dublette löschen","%n Dubletten löschen"],
     "Log entries fetched" : "Log Einträge eingelesen",
     "Could not fetch log entries" : "Log Einträge konnten nicht eingelesen werden",
     "Could not fetch settings" : "Einstellungen nicht eingelesen werden",
--- l10n/en_GB.js
+++ l10n/en_GB.js
@@ -4,6 +4,7 @@ OC.L10N.register(
     "How many lines to display" : "How many lines to display",
     "How many characters should the error reason display" : "How many characters should the error reason display",
     "Delete dublicates" : "Delete dublicates",
+    "_Delete %n dublicate_::_Delete %n dublicates_" : ["Delete %n dublicate","Delete %n dublicates"],
     "Log entries fetched" : "Log entries fetched",
     "Could not fetch log entries" : "Could not fetch log entries",
     "Could not fetch settings" : "Could not fetch settings",
@@ -29,7 +30,7 @@ OC.L10N.register(
     "URL" : "URL",
     "right" : "right",
     "top" : "top",
-    "Time offset in hours, e.g. due to winter time" : "Time offset in hours, e.g. due to winter time",    
+    "Time offset in hours, e.g. due to winter time" : "Time offset in hours, e.g. due to winter time",
     "which menue to use" : "which menue to use",
     "(only effective after the next page refresh)" : "(only effective after the next page refresh)"
 },
