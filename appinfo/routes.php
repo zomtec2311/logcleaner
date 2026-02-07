6ca725592fe315d57ef5f6a96582ab5e7ea54711
zomtec2311 <wtoedt@gmail.com>
2026-02-07 09:16:57 +0100

    Add files via upload

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -39,6 +39,7 @@ return [
     ['name' => 'Settings#countDub', 'url' => '/countdub', 'verb' => 'GET'],
     ['name' => 'Settings#logapps', 'url' => '/logapps', 'verb' => 'GET'],
     ['name' => 'Settings#dellog', 'url' => '/dellog/{logid}', 'verb' => 'GET'],
+    ['name' => 'Settings#delLevel', 'url' => '/delLevel/{level}', 'verb' => 'GET'],
     ['name' => 'Settings#dellog', 'url' => '/dellog/{logid}', 'verb' => 'GET'],
     ['name' => 'Settings#setSettingZeilen', 'url' => '/setlines/{who}/{zeilen}', 'verb' => 'GET'],
     ['name' => 'Settings#logfileandsize', 'url' => '/logfileandsize', 'verb' => 'GET'],
